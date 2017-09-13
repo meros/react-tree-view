53c10d4261a62c5a1e98b7d86a7f2567c66a362b
Alexander Schrab <alexander.schrab@gmail.com>
2017-09-13 21:37:03 +0200

    Keyboard shortcuts

--- react-tree-view/src/components/HelpModal.js
+++ react-tree-view/src/components/HelpModal.js
@@ -5,17 +5,47 @@ import './HelpModal.css';
 import React, { Component } from 'react';
 
 import Draggable from 'react-draggable';
+import cx from 'classnames';
 
 // TODO: make nice/complete
 
 export default class HelpModal extends Component {
   render() {
+    const info = [
+      ['Focus next', 'Arrow down'],
+      ['Focus prev', 'Arrow up'],
+      ['Expand', 'Meta + arrow down'],
+      ['Collapse', 'Meta + arrow up'],
+      ['Create new', 'Enter'],
+      ['Drag up', 'Shift + meta + arrow up'],
+      ['Drag down', 'Shift + meta + arrow down'],
+      ['Indent', 'Tab'],
+      ['Outdent', 'Shift + tab'],
+      ['Mark complete', 'Meta + enter'],
+      ['Zoom in', 'Meta + arrow right'],
+      ['Zoom out', 'Meta + arrow left'],
+    ];
+
     return (
       <Draggable>
         <div className="HelpModal">
-          Arrow up/down: select next/prev node<br />
-          Enter: create new node<br />
-          etc...
+          <div className="HelpModal__header">Keyboard shortcuts</div>
+          <table className="HelpModal__gridcontainer">
+            {info.map((row, y) => (
+              <tr>
+                {row.map((cell, x) => (
+                  <td>
+                    <div
+                      className={cx('HelpModal__cellcontent', {
+                        'HelpModal__cellcontent--odd': y % 2 !== 0,
+                      })}>
+                      {cell}
+                    </div>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </table>
         </div>
       </Draggable>
     );
